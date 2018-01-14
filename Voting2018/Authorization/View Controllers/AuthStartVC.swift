1909cf0aa00a9f5d01341fbc5556832c6af54e65
Андрей Рожков <andreylanadelrey@MacBook-Pro-Andrej.local>
2018-01-15 00:40:35 +0300

    add auth vc and soryboard, add git ignore

---
+++ Voting2018/Authorization/View Controllers/AuthStartVC.swift
@@ -0,0 +1,33 @@
+//
+//  AuthStartVC.swift
+//  Voting2018
+//
+//  Created by Андрей Рожков on 15.01.2018.
+//  Copyright © 2018 Andrey Rozhkov. All rights reserved.
+//
+
+import UIKit
+
+class AuthStartVC: UIViewController {
+
+    @IBOutlet var buttonVKAuth: UIButton!
+    @IBOutlet var buttonOKAuth: UIButton!
+    
+    
+    //MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    //MARK: Actions
+    
+    @IBAction func actionButtonVKAuth(_ sender: Any) {
+    }
+    
+    @IBAction func actionButtonOKAuth(_ sender: Any) {
+    }
+    
+}
---
+++ Voting2018/Managers/ScreensManager.swift
@@ -0,0 +1,31 @@
+//
+//  ScreensManager.swift
+//  Voting2018
+//
+//  Created by Андрей Рожков on 15.01.2018.
+//  Copyright © 2018 Andrey Rozhkov. All rights reserved.
+//
+
+import UIKit
+
+class ScreensManager {
+    
+    static let sI = ScreensManager()
+    
+    
+    func showAuthFlow() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: "Authorization", bundle: nil)
+        let nController = storyboard.instantiateViewController(withIdentifier :"StartNC") as! UINavigationController
+        
+        appDelegate.window?.rootViewController = nController
+        appDelegate.window?.makeKeyAndVisible()
+        
+}
+
+
+
+}
